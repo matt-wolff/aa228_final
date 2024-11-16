69eb6bb71d6b38877277dab864acd23a1c900ce6
matt-wolff <mbwolff@stanford.edu>
2024-11-15 16:50:45 -0800

    First pass of training loop.

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import torch
+from torch.optim import AdamW
 import torch.nn as nn
 import numpy as np
 import random
@@ -8,12 +8,13 @@ STATE_DIM = 135  # First thirteen are wild cards
 ACTION_DIM = 11  # check, call, fold, raise (2x, 3x, 4x highest bet. 1/4, 1/2, 1, 2 pot. all-in)
 
 class PokerModel(nn.Module):
-    def __init__(self,
-                 hidden_dim,
-                 n_layers,
-                 dropout_p,
-                 vanilla  # If true, don't consider the first 
-                 ):
+    def __init__(
+            self,
+            hidden_dim,
+            n_layers,
+            dropout_p,
+            vanilla  # If true, don't consider the first 
+        ):
         self.vanilla = vanilla
 
         layers = []
@@ -27,17 +28,22 @@ class PokerModel(nn.Module):
             prev_dim = hidden_dim
         layers.append(nn.Linear(prev_dim, ACTION_DIM))
         self.model = nn.Sequential(layers)
+        self.optimizer = AdamW(self.model.parameters())
 
         self.epsilion = 1
         self.epsilon_lr = 1 - 10**-4
+
+    def max_action(self, state_vector):
+        logits = self.model(state_vector)
+        action = np.argmax(logits)
+        return action, logits[action]
 
     def next_action(self, state_vector):
         if self.vanilla:
             state_vector = state_vector[NUM_WILD_CARDS:]
         random_action = random.random() < self.epsilion
-        if random_action:
-            return random.randint(0, ACTION_DIM-1)
-        logits = self.model(state_vector)
         self.epsilion = self.epsilion * self.epsilon_lr
-        return np.argmax(logits)
+        if random_action:
+            return random.randint(0, ACTION_DIM-1), None
+        return self.max_action(state_vector)
 
---
+++ train.py
@@ -0,0 +1,74 @@
+import torch
+from model import PokerModel
+from poker import Game
+import copy
+import random
+
+NUM_ACTIONS = 10000  # Number of actions to take
+BATCH_SIZE = 10
+GAMMA = 1
+
+def main():
+    agent = PokerModel(hidden_dim=1024, n_layers=5, dropout_p=0.5, vanilla=False)
+    target_agent = agent
+
+    game = Game()
+    replay_buffer = []
+    losses = []
+
+    transtion_construction = [
+        [],
+        []
+    ]
+
+    for i in range(len(NUM_ACTIONS)):
+        current_player = game.current_to_act
+        current_state = next_state
+        if transtion_construction[current_player]:
+            transtion_construction[current_player].append(current_state)
+            replay_buffer = replay_buffer[1:] + [transtion_construction[current_player]]
+            transtion_construction[current_player] = []
+
+        action, _ = agent.next_action(current_state)
+        reward, game_finished = game.perform_action(action)
+
+        if game_finished:
+            cur_player_transition = [current_state, action, reward, None]
+            replay_buffer = replay_buffer[1:] + [cur_player_transition]
+            game = Game()
+            next_state = game.get_state()
+            # TODO: update last transition from other player
+        else:
+            next_state = game.get_state()
+            next_player = game.current_to_act
+            if next_player != current_player:
+                transtion_construction[current_player] = [current_state, action, reward]
+            else:
+                replay_buffer = replay_buffer[1:] + [[current_state, action, reward, next_state]]
+        
+        if len(replay_buffer) > 10:
+            minibatch = random.sample(replay_buffer, BATCH_SIZE)
+
+            loss = 0
+            for transition in minibatch:
+                s_t, a_t, r_t, s_t_plus_one = transition
+                if s_t_plus_one == None:
+                    y = r_t
+                else:
+                    with torch.no_grad():
+                        _, target_val = target_agent.max_action(s_t_plus_one)
+                        y = r_t + GAMMA * target_val
+                loss += (y - agent.model(s_t)[a_t]) ** 2
+
+            agent.optimizer.zero_grad()
+            loss.backward()
+            agent.optimizer.step()
+
+            losses.append(loss.float())
+
+        if (i+1 % 1000 == 0):
+            target_agent = copy.deepcopy(agent)
+
+
+if __name__ == "__main__":
+    main()
